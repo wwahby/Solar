459de403e4361bafbc97fdfc7d21b81d0e418de7
wwahby <w.wahby@gmail.com>
2015-10-20 17:07:39 -0400

    changed nvol script to function

--- calc_nvol.m
+++ calc_nvol.m
@@ -1,13 +1,10 @@
-%function nvol = calc_nvol(phi_o_cm2, r_m, alpha_cm, zo_m)
+function nvol = calc_nvol(lambda_nm, r_m, zo_m)
 
-lambda_nm = 500;
-r_m = 2e-6;
-zo_m = 20e-6;
 
 [flux_per_m2, flux_wehrli_per_m2] = vsc.get_photon_flux_per_m2( lambda_nm );
 alpha_cm = vsc.get_absorption_coefficient(lambda_nm);
 
-phi_o_cm2 = flux_per_m2 /1e4;
+phi_o_cm2 = flux_per_m2/1e4;
 
 %% Conversions
 r = r_m*1e2;
@@ -25,4 +22,4 @@ nvol_c = exp(-alpha*r)*(alpha^2*r^2 + 2*alpha*r + 2);
 nvol_d = exp(alpha*r) *(alpha^2*r^2 - 2*alpha*r + 2);
 
 %% Combine
-nvol = nvol_a + nvol_b*(nvol_c - nvol_d)
+nvol = nvol_a + nvol_b*(nvol_c - nvol_d);
