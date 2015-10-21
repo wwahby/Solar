886e750b5996a8bc06106a331a5b68310a10236a
wwahby <w.wahby@gmail.com>
2015-10-21 10:49:47 -0400

    Nvol calculation now also includes photons captured per wavelength (TEST)

--- calc_nvol.m
+++ calc_nvol.m
@@ -1,7 +1,7 @@
-function nvol = calc_nvol(lambda_nm, r_m, zo_m)
+function [nvol, nvol_per_nm] = calc_nvol(lambda_nm, r_m, zo_m)
 
 
-[flux_per_m2, flux_wehrli_per_m2] = vsc.get_photon_flux_per_m2( lambda_nm );
+[flux_per_m2, flux_wehrli_per_m2, irradiance_per_m2nm, irradiance_wehrli_per_m2nm] = vsc.get_photon_flux_per_m2( lambda_nm );
 alpha_cm = vsc.get_absorption_coefficient(lambda_nm);
 
 phi_o_cm2 = flux_per_m2/1e4;
@@ -21,5 +21,15 @@ nvol_b = pi/alpha^2*phi_o*exp(-alpha*zo);
 nvol_c = exp(-alpha*r)*(alpha^2*r^2 + 2*alpha*r + 2);
 nvol_d = exp(alpha*r) *(alpha^2*r^2 - 2*alpha*r + 2);
 
+%% Calculations
+phi_o_cm2nm = irradiance_per_m2nm/1e4;
+phi_o = phi_o_cm2nm;
+
+nvol_per_nm_a = pi*r^2*phi_o*(exp(-alpha*zt) - exp(-alpha*zb));
+nvol_per_nm_b = pi/alpha^2*phi_o*exp(-alpha*zo);
+nvol_per_nm_c = exp(-alpha*r)*(alpha^2*r^2 + 2*alpha*r + 2);
+nvol_per_nm_d = exp(alpha*r) *(alpha^2*r^2 - 2*alpha*r + 2);
+
 %% Combine
 nvol = nvol_a + nvol_b*(nvol_c - nvol_d);
+nvol_per_nm_ = nvol_per_nm_a + nvol_per_nm_b*(nvol_per_nm_c - nvol_per_nm_d);
