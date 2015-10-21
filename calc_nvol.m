c83de6418082374afb64970ed6734b15564ff655
wwahby <w.wahby@gmail.com>
2015-10-21 17:30:27 -0400

    Added alternate Nvol calculations for comparison (G_per_nm is correct one)

--- calc_nvol.m
+++ calc_nvol.m
@@ -1,7 +1,7 @@
-function [nvol, nvol_per_nm] = calc_nvol(lambda_nm, r_m, zo_m)
+function [nvol, nvol_per_nm, G_per_nm, flux_per_m2, flux_per_m2nm, irradiance_per_m2nm] = calc_nvol(lambda_nm, r_m, zo_m)
 
 
-[flux_per_m2, flux_wehrli_per_m2, irradiance_per_m2nm, irradiance_wehrli_per_m2nm] = vsc.get_photon_flux_per_m2( lambda_nm );
+[flux_per_m2, flux_wehrli_per_m2, flux_per_m2nm, flux_wehrli_per_m2nm, irradiance_per_m2nm, irradiance_wehrli_per_m2nm] = vsc.get_photon_flux_per_m2( lambda_nm );
 alpha_cm = vsc.get_absorption_coefficient(lambda_nm);
 
 phi_o_cm2 = flux_per_m2/1e4;
@@ -15,6 +15,9 @@ phi_o = phi_o_cm2;
 zt = zo-r;
 zb = zo+r;
 
+za = zt;
+zb = zb;
+
 %% Calculations
 nvol_a = pi*r^2*phi_o*(exp(-alpha*zt) - exp(-alpha*zb));
 nvol_b = pi/alpha^2*phi_o*exp(-alpha*zo);
@@ -22,7 +25,7 @@ nvol_c = exp(-alpha*r)*(alpha^2*r^2 + 2*alpha*r + 2);
 nvol_d = exp(alpha*r) *(alpha^2*r^2 - 2*alpha*r + 2);
 
 %% Calculations
-phi_o_cm2nm = irradiance_per_m2nm/1e4;
+phi_o_cm2nm = flux_per_m2nm/1e4;
 phi_o = phi_o_cm2nm;
 
 nvol_per_nm_a = pi*r^2*phi_o*(exp(-alpha*zt) - exp(-alpha*zb));
@@ -30,6 +33,14 @@ nvol_per_nm_b = pi/alpha^2*phi_o*exp(-alpha*zo);
 nvol_per_nm_c = exp(-alpha*r)*(alpha^2*r^2 + 2*alpha*r + 2);
 nvol_per_nm_d = exp(alpha*r) *(alpha^2*r^2 - 2*alpha*r + 2);
 
+G_per_nm  = 2*pi/alpha^2*phi_o * ( exp(-alpha*zb)*(alpha*r + 1) + exp(-alpha*za)*(alpha*r-1) ); % total generation rate per wavelength in spherical vol
+
+
 %% Combine
 nvol = nvol_a + nvol_b*(nvol_c - nvol_d);
 nvol_per_nm = nvol_per_nm_a + nvol_per_nm_b*(nvol_per_nm_c - nvol_per_nm_d);
+
+
+%% Clear NaNs?
+%nvol( isnan(nvol) ) = 0;
+%nvol_per_nm( isnan(nvol_per_nm) ) = 0;
