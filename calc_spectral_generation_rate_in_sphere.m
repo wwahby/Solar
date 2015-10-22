02d9440d2ff076d94f1b90c1a020ed26cc58c201
wwahby <w.wahby@gmail.com>
2015-10-22 11:15:30 -0400

    Spherical generation rate now properly accounts for clipped spheres
    Only deals with boundary nearest to the surface -- still assume other
    boundary is never clipped.

--- calc_spectral_generation_rate_in_sphere.m
+++ calc_spectral_generation_rate_in_sphere.m
@@ -1,6 +1,6 @@
 function [G_per_nm, flux_per_m2nm, irradiance_per_m2nm, alpha_cm] = calc_spectral_generation_rate_in_sphere(lambda_nm, r_m, zo_m)
 %% Calculates carrier generation rate in a sphere of radius r, centered on depth zo, for illumination of wavelength lambda
-% Assumes zo-r > 0
+
 
 [flux_per_m2, flux_wehrli_per_m2, flux_per_m2nm, flux_wehrli_per_m2nm, irradiance_per_m2nm, irradiance_wehrli_per_m2nm] = vsc.get_photon_flux_per_m2( lambda_nm );
 alpha_cm = vsc.get_absorption_coefficient(lambda_nm);
@@ -13,7 +13,19 @@ alpha = alpha_cm;
 za = zo-r;
 zb = zo+r;
 
-%% Calculations
 phi_o_cm2nm = flux_per_m2nm/1e4;
 phi_o = phi_o_cm2nm;
-G_per_nm  = 2*pi/alpha^2*phi_o * ( exp(-alpha*zb)*(alpha*r + 1) + exp(-alpha*za)*(alpha*r-1) ); % total generation rate per wavelength in spherical vol
+
+%% Calculations
+% Simplified expression for full sphere -- not necessary since partial
+% sphere expression is also very compact
+%G_per_nm  = 2*pi/alpha^2*phi_o * ( exp(-alpha*zb)*(alpha*r + 1) + exp(-alpha*za)*(alpha*r-1) ); % total generation rate per wavelength in spherical vol
+
+% Clip collection sphere if it extends past surface
+if (zo < r)
+    za = 0;
+end
+
+G_per_nm_A = exp(-alpha*za)*(alpha^2*(r^2-(zo-za)^2) + 2*alpha*(zo-za)-2);
+G_per_nm_B = exp(-alpha*zb)*(alpha^2*(r^2-(zo-zb)^2) + 2*alpha*(zb-zo)+2);
+G_per_nm = pi/alpha^2*phi_o * (G_per_nm_A  + G_per_nm_B);
